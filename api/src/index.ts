98a0d45dd0c0c5c2a00ccdea2a157c87322a575f
Tonysw2 <anthonyrmayaa@gmail.com>
2023-12-14 11:58:37 -0300

    feat: implement WebSocket in API to emit events on new order addition

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,24 @@
-import path from 'node:path'
 import express, { json } from 'express'
 import mongoose from 'mongoose'
+import http from 'node:http'
+import path from 'node:path'
 import { router } from './router'
+import { Server } from 'socket.io'
+
+const app = express()
+const server = http.createServer(app)
+export const io = new Server(server)
 
 mongoose.connect('mongodb://localhost:27017')
   .then(res => {
-    const app = express()
     const port = 3001
 
+    app.use((req, res, next) => {
+      res.setHeader('access-control-allow-origin', '*')
+      res.setHeader('access-control-allow-methods', '*')
+      res.setHeader('access-control-allow-headers', '*')
+      next()
+    })
     app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
     app.use(json())
     app.use(router)
@@ -16,7 +27,7 @@ mongoose.connect('mongodb://localhost:27017')
       res.send('<h1>Welcome to WaiterApp api</h1>')
     })
 
-    app.listen(port, () => {
+    server.listen(port, () => {
       console.log(`Server running on http://localhost:${port}`)
     })
   })
